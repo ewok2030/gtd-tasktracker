1afc624cf4ddbcb10c188f99fe9fdeb35b7f2317
ewok2030 <mpalumbo7@gmail.com>
2016-12-06 17:45:03 -0500

    Integrated mongoose for accessing data in MongoDB

---
+++ server/data/Project.seed.js
@@ -0,0 +1,20 @@
+import Project from '../models/Project.model';
+
+export default function() {
+    Project.count().exec((err, count) => {
+        if (count > 0) {
+            return;
+        }
+        console.log("Seeding Projects...")
+        const projectA = new Project({name: 'Test Project A'});
+        const projectB = new Project({name: 'Test Project B'});
+
+        Project.create([
+            projectA, projectB
+        ], (error) => {
+            if (!error) {
+                // console.log('ready to go....');
+            }
+        });
+    });
+}
---
+++ server/routes/Project.routes.js
@@ -0,0 +1,17 @@
+import {Router} from 'express';
+import * as projectController from '../controllers/Project.controller';
+const router = new Router();
+
+// Get All
+router.route('/').get(projectController.getProjects);
+
+// Get
+router.route('/:id').get(projectController.getProject);
+
+// Create
+router.route('/').post(projectController.addProject);
+
+// Delete
+router.route('/:id').delete(projectController.deleteProject);
+
+export default router;
