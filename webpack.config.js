7647792717bbbc0f4ec8147a1051b7bcd9915655
ewok2030 <ewok2030@users.noreply.github.com>
2016-12-06 00:05:00 -0500

    Time to merge to master (#1)
    * Incoprorated the mern-starter configuration
    
    * Reduced the complecity of the project
    
    * Added nodemon to auto reload server changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,46 @@
-var debug = process.env.NODE_ENV !== "production";
-var webpack = require('webpack');
+'use strict';
+
 var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    context: path.join(__dirname, "src"),
-    entry: "./client/client.jsx",
+    devtool: 'eval-source-map',
+    entry: [
+        'webpack-hot-middleware/client?reload=true',
+        path.join(__dirname, 'client/client.jsx')
+    ],
     output: {
-        path: path.join(__dirname, "public"),
-        publicPath: "/js/",
-        filename: "client.bundle.js"
+        path: path.join(__dirname, '/dist/'),
+        filename: '[name].js',
+        publicPath: '/'
     },
-
-    devtool: debug
-        ? "inline-sourcemap"
-        : null,
-
+    plugins: [
+        new HtmlWebpackPlugin({template: 'client/templates/index.html', inject: 'body', filename: 'index.html', 'favicon': 'client/templates/favicon.ico'}),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin(),
+        new webpack.DefinePlugin({'process.env.NODE_ENV': JSON.stringify('development')})
+    ],
     module: {
         loaders: [
             {
                 test: /\.jsx?$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'babel-loader',
+                exclude: /node_modules/,
+                loader: 'babel',
                 query: {
-                    presets: [
-                        'react', 'es2015', 'stage-0'
+                    "presets": [
+                        "react", "es2015", "stage-0", "react-hmre"
                     ],
                     plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties']
                 }
             }, {
-                test: /\.json$/,
-                loader: 'json-loader'
+                test: /\.json?$/,
+                loader: 'json'
+            }, {
+                test: /\.css$/,
+                loader: 'style!css?modules&localIdentName=[name]---[local]---[hash:base64:5]'
             }
         ]
-    },
-
-    plugins: debug
-        ? []
-        : [
-            new webpack.optimize.DedupePlugin(),
-            new webpack.optimize.OccurenceOrderPlugin(),
-            new webpack.optimize.UglifyJsPlugin({mangle: false, sourcemap: false})
-        ]
+    }
 };
