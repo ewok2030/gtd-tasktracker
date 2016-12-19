e96a2c8ecf6bf2dc86eb9b2d3027e38ea8149328
Michael Palumbo <20686457+mpalumbo7@users.noreply.github.com>
2016-12-18 23:50:58 -0500

    Working on Today task boards

--- client/views/Today.js
+++ client/views/Today.js
@@ -1,9 +1,48 @@
-import React from "react";
+import React from 'react';
+import {connect} from 'react-redux';
+// Components
+import TaskBoard from '../components/TaskBoard/TaskBoard';
+// Actions
+import {fetchTasks} from '../actions/fetchTasks.action';
 
+// Map store state to component's properties
+const mapStateToProps = (state, ownProps) => {
+    return {
+        newTasks: state.fetchTasks.data.filter(t => t.status === "New"),
+        committedTasks: state.fetchTasks.data.filter(t => t.status === "Open"),
+        activeTasks: state.fetchTasks.data.filter(t => t.status === "Active"),
+        completedTasks: state.fetchTasks.data.filter(t => t.status === "Completed")
+    };
+};
+
+// Map actions to component's properties
+const mapDispatchToProps = (dispatch, ownProps) => {
+    return {
+        fetchTasks: () => {
+            dispatch(fetchTasks());
+        }
+    }
+};
+
+@connect(mapStateToProps, mapDispatchToProps)
 export default class Today extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            viewTitle: "Today"
+        };
+    }
+
+    componentWillMount() {
+        this.props.fetchTasks();
+    }
+
     render() {
         return (
-            <h1>Today</h1>
+            <div>
+                <h2>{this.state.viewTitle}</h2>
+                <TaskBoard newTasks={this.props.newTasks} committedTasks={this.props.committedTasks} activeTasks={this.props.committedTasks} completedTasks={this.props.committedTasks} />
+            </div>
         );
     }
 }
