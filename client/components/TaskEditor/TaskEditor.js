0fd9a657b42665748bca8dc488cb37fb202567d2
Michael Palumbo <20686457+mpalumbo7@users.noreply.github.com>
2016-12-27 00:17:59 -0500

    Added validation to the task form

--- client/components/TaskEditor/TaskEditor.js
+++ client/components/TaskEditor/TaskEditor.js
@@ -1,33 +1,31 @@
 import React from 'react';
 // import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
+import { validate, warn } from './taskValidation';
 
-const renderField = field => (
-  <div className="form-group">
-    <label htmlFor={field.label}>{field.label}</label>
-    <input {...field.input} className="form-control" />
-    {field.touched && field.error && <div className="error">{field.error}</div>}
-  </div>
-);
+// Form Fields
+const renderField = (field, element) => {
+  const hasIssue = (field.meta.touched && ((field.meta.error && 'has-error') || (field.meta.warning && 'has-warning')));
+  return (
+    <div className={`form-group ${hasIssue}`}>
+      <label htmlFor={field.label}>{field.label}</label>
+      {element}
+      {field.meta.touched && (
+        (field.meta.error &&
+          <span className="help-block"><span className="glyphicon glyphicon-exclamation-sign" aria-hidden="true" />&nbsp;{field.meta.error}</span>
+        ) || (field.meta.warning &&
+          <span className="help-block"><span className="glyphicon glyphicon-exclamation-sign" aria-hidden="true" />&nbsp;{field.meta.warning}</span>)
+      )}
+    </div>);
+};
 
-const renderSelect = field => (
-  <div className="form-group">
-    <label htmlFor={field.label}>{field.label}</label>
-    <select {...field.input} className="form-control">
-      {field.children}
-    </select>
-    {field.touched && field.error && <div className="error">{field.error}</div>}
-  </div>
-);
+const renderInput = field => (renderField(field, <input {...field.input} className="form-control" />));
 
-const renderTextArea = field => (
-  <div className="form-group">
-    <label htmlFor={field.label}>{field.label}</label>
-    <textarea {...field.input} className="form-control" />
-  </div>
-);
+const renderTextArea = field => (renderField(field, <textarea {...field.input} className="form-control" />));
 
-@reduxForm({ form: 'TaskEditor', enableReinitialize: true })
+const renderSelect = field => (renderField(field, <select {...field.input} className="form-control">{field.children}</select>));
+
+@reduxForm({ form: 'TaskEditor', validate, warn, enableReinitialize: true })
 export default class TaskEditor extends React.Component {
   static propTypes = {
     onSave: React.PropTypes.func.isRequired,
@@ -43,10 +41,11 @@ export default class TaskEditor extends React.Component {
     reset: React.PropTypes.func.isRequired,
     pristine: React.PropTypes.bool.isRequired,
     invalid: React.PropTypes.bool.isRequired,
+    submitting: React.PropTypes.bool.isRequired,
   }
 
   render() {
-    const { pristine, reset, invalid, handleSubmit, onSave } = this.props;
+    const { pristine, reset, invalid, submitting, handleSubmit, onSave } = this.props;
     return (
       <div className="panel panel-default">
         <div className="panel-heading">
@@ -58,7 +57,7 @@ export default class TaskEditor extends React.Component {
         <div className="panel-body">
           <form onSubmit={handleSubmit(onSave)}>
 
-            <Field name="title" type="text" component={renderField} label="Title" />
+            <Field name="title" type="text" component={renderInput} label="Title" />
 
             <Field name="status" component={renderSelect} label="Status">
               {this.props.statusList.map(s => <option key={s._id} value={s.label}>{s.label}</option>)}
@@ -67,7 +66,7 @@ export default class TaskEditor extends React.Component {
             <Field name="description" component={renderTextArea} label="Description" />
 
             <button action="submit" disabled={pristine || invalid} className="btn btn-primary">Submit</button>
-            <button type="button" disabled={pristine} onClick={reset} className="btn btn-default">Undo</button>
+            <button type="button" disabled={pristine || submitting} onClick={reset} className="btn btn-default">Undo</button>
           </form>
         </div>
         <div className="panel-footer" />
